b752bdcebd0a88cd34acbdf172da0b3d5d2fa432
Morteza Masumi <morteza.masumi@gmail.com>
2025-08-13 16:29:15 +0330

    fix: minor

--- src/Facades/FbSetting.php
+++ src/Facades/FbSetting.php
@@ -5,6 +5,8 @@ namespace Mortezamasumi\FbSetting\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static mixed get(string $key, mixed $default = null, ?string $attrKey = null, array $values = [])
+ *
  * @see \Mortezamasumi\FbSetting\FbSetting
  */
 class FbSetting extends Facade
