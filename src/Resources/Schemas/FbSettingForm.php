8974634855f142db280371d4da508f01c57f368b
Morteza Masumi <morteza.masumi@gmail.com>
2025-09-24 19:58:03 +0330

    fix: minor in setting form to not show browser validation

--- src/Resources/Schemas/FbSettingForm.php
+++ src/Resources/Schemas/FbSettingForm.php
@@ -5,6 +5,7 @@ namespace Mortezamasumi\FbSetting\Resources\Schemas;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Schema;
 
 class FbSettingForm
@@ -15,24 +16,29 @@ class FbSettingForm
             ->components([
                 TextInput::make('key')
                     ->label(__('fb-setting::fb-setting.form.key'))
-                    ->required()
+                    ->rules(['required'])
+                    ->markAsRequired()
                     ->unique(ignoreRecord: true)
                     ->maxLength(255)
                     ->extraAlpineAttributes(['dir' => 'ltr']),
                 Textarea::make('value')
                     ->label(__('fb-setting::fb-setting.form.value'))
                     ->requiredWithout('attributes')
+                    ->markAsRequired()
                     ->maxLength(2048)
                     ->rows(2),
                 Repeater::make('attributes')
                     ->label(__('fb-setting::fb-setting.form.attributes'))
                     ->requiredWithout('value')
+                    ->live()
                     ->schema([
                         TextInput::make('key')
                             ->label(__('fb-setting::fb-setting.form.key'))
+                            ->required()
                             ->maxLength(255),
                         TextInput::make('value')
                             ->label(__('fb-setting::fb-setting.form.value'))
+                            ->required()
                             ->maxLength(255),
                     ])
                     ->default([])
