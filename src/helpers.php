f459ae5f4583f0484dcf6dbbda147d2bb6c8c81b
Morteza Masumi <morteza.masumi@gmail.com>
2025-10-28 07:07:53 +0330

    fix: add helpers to autoload in composer.json

--- src/helpers.php
+++ src/helpers.php
@@ -2,8 +2,8 @@
 
 use Mortezamasumi\FbSetting\Facades\FbSetting;
 
-if (! function_exists('__setting')) {
-    function __setting(string $key, mixed $default = null, ?string $attrKey = null, array $values = []): string
+if (! function_exists('__fb_setting')) {
+    function __fb_setting(string $key, mixed $default = null, ?string $attrKey = null, array $values = []): string
     {
         return FbSetting::get($key, $default, $attrKey, $values);
     }
