9990f8ed43a75a83344822bc8753211edc4adae5
Morteza Masumi <morteza.masumi@gmail.com>
2025-12-09 18:39:18 +0330

    fix: minor in test

--- tests/Tests/SettingResourceTest.php
+++ tests/Tests/SettingResourceTest.php
@@ -4,24 +4,27 @@ use Illuminate\Support\Facades\Gate;
 use Mortezamasumi\FbSetting\Facades\FbSetting;
 use Mortezamasumi\FbSetting\Models\FbSetting as ModelsFbSetting;
 use Mortezamasumi\FbSetting\Policies\FbSettingPolicy;
-use Mortezamasumi\FbSetting\Resources\FbSettingResource;
 use Mortezamasumi\FbSetting\Resources\Pages\ManageFbSettings;
+use Mortezamasumi\FbSetting\Resources\FbSettingResource;
 use Mortezamasumi\FbSetting\Tests\Services\User;
 
 beforeEach(function () {
-    Gate::before(fn () => true);
+    Gate::before(fn() => true);
     Gate::policy(ModelsFbSetting::class, FbSettingPolicy::class);
 
+    /** @var Pest $this */
     $this->actingAs(User::factory()->create());
 });
 
 it('can render index page', function () {
+    /** @var Pest $this */
     $this
         ->get(FbSettingResource::getUrl('index'))
         ->assertSuccessful();
 });
 
 it('can show empty table', function () {
+    /** @var Pest $this */
     $this
         ->livewire(ManageFbSettings::class)
         ->assertCanSeeTableRecords([])
@@ -38,6 +41,7 @@ it('can show data in table', function () {
         ]],
     ]);
 
+    /** @var Pest $this */
     $this
         ->livewire(ManageFbSettings::class)
         ->assertCountTableRecords(1);
@@ -49,6 +53,7 @@ it('can create key-value setting', function () {
         'value' => 'test value',
     ];
 
+    /** @var Pest $this */
     $this
         ->livewire(ManageFbSettings::class)
         ->mountAction('create')
@@ -76,6 +81,7 @@ it('can create key-attribute setting using facade', function () {
         ]
     ];
 
+    /** @var Pest $this */
     $this
         ->livewire(ManageFbSettings::class)
         ->mountAction('create')
@@ -89,6 +95,7 @@ it('can create key-attribute setting using facade', function () {
 });
 
 it('can get validation error', function () {
+    /** @var Pest $this */
     $this
         ->livewire(ManageFbSettings::class)
         ->mountAction('create')
@@ -107,6 +114,7 @@ it('can edit setting', function () {
         'value' => 'test value',
     ]);
 
+    /** @var Pest $this */
     $this
         ->livewire(ManageFbSettings::class)
         ->mountTableAction('edit', $record)
